713ffb87e8681003233a9e9fea4b739843bca12d
vickykimani <victorianightingale70@gmail.com>
2022-02-24 12:20:25 +0300

    per-pixel lighting

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -5,6 +5,7 @@ import org.lwjgl.util.vector.Vector3f;
 
 import entities.Camera;
 import entities.Entity;
+import entities.Light;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
@@ -25,11 +26,12 @@ public class MainGameLoop {
 		Renderer renderer = new Renderer(shader);
 		
 				
-		RawModel model = OBJLoader.loadObjModel("stall", loader);
+		RawModel model = OBJLoader.loadObjModel("dragon", loader);
 				
-		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
+		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("white")));
 		
-		Entity entity = new Entity(staticModel, new Vector3f(0,0,-50),0,0,0,1);
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-25),0,0,0,1);
+		Light light = new Light(new Vector3f(0,0,-20), new Vector3f(1,1,1));
 		
 		Camera camera = new Camera();
 		
@@ -39,6 +41,7 @@ public class MainGameLoop {
 			camera.move();
 			renderer.prepare();
 			shader.start();
+			shader.loadLight(light);
 			shader.loadViewMatrix(camera);
 			renderer.render(entity,shader);
 			shader.stop();
